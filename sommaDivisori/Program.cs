b2294b79865c5f91bc4c24c909222ecb219b7965
manciolove <114284283+manciolove@users.noreply.github.com>
2025-10-28 10:44:18 +0100

    Aggiungere i file di progetto.

---
+++ sommaDivisori/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace sommaDivisori
+{
+    internal class Program
+    {
+        static bool Nprimo(int n)
+        {
+            
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        static int Somma(int n)
+        {
+            int s = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+
+                    if (Nprimo(n) == true)
+                    {
+                        s = s + i;
+                    }
+                }
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero: ");
+            int a = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("la somms dei divisori primi: ");
+        }
+    }
+}
